127b192b57199a608ba2e74b0d2e4e4f789fa3f7
kosail <kamururi@pm.me>
2023-10-04 08:02:44 -0600

    fixing bugs

--- unidad2/FPU2Prob7_DFPS.java
+++ unidad2/FPU2Prob7_DFPS.java
@@ -8,16 +8,15 @@ public class FPU2Prob7_DFPS {
     public static void main(String[] args) {
         int userNum = 0;
 
-        userNum = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un número de dos dígitos:"));
+        do {
+            userNum = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un número de dos dígitos:"));
+        } while(Math.abs(userNum)>99 || Math.abs(userNum)<10);
 
-        if (userNum>=-99 && userNum<=-10) {
-            if (userNum%2!=0 && userNum<0) {
+
+        if (userNum%2!=0 && userNum<0) {
                 JOptionPane.showMessageDialog(null,"El número es primo y es negativo.");
             } else {
                 JOptionPane.showMessageDialog(null,"El número no cumple con las condiciones de ser primo y negativo.");
-            }
-        } else {
-            JOptionPane.showMessageDialog(null,"El número que ingresaste tiene más o menos de 2 dígitos.\nVuelve a intentarlo.");
         }
 
     }
