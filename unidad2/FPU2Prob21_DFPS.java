f3802fef33dbaf648a41a2b1c708e111b6b6bce1
kosail <kamururi@pm.me>
2023-10-10 07:35:17 -0600

    updating last exercises

--- unidad2/FPU2Prob21_DFPS.java
+++ unidad2/FPU2Prob21_DFPS.java
@@ -6,17 +6,25 @@ import java.util.Scanner;
 
 public class FPU2Prob21_DFPS {
     public static void main(String[] args) {
-        int ratePerHour=0, amountToPay=0;
-        float hoursStayed=0;
+        int ratePerHour=0, amountToPay=0, hoursStayed=0, minutesStayed=0;
         ratePerHour = 20;
-        Scanner scannedHoursStayed = new Scanner (System.in);
+        Scanner scannedTimeStayed = new Scanner (System.in);
 
         System.out.print("+-----------------------------------+\nCalculadora de costos de parking\n+-----------------------------------+\n*Considera que las fracciones de hora se toman como completas*\n\nIngresa la cantidad de horas que estuviste en el estacionamiento: ");
-        hoursStayed = scannedHoursStayed.nextFloat();
-        
-        scannedHoursStayed.close();
-        amountToPay = Math.round(ratePerHour*hoursStayed);
+        hoursStayed = scannedTimeStayed.nextInt();
+
+        do {
+            System.out.print("Ingresa la cantidad de minutos: ");
+            minutesStayed = scannedTimeStayed.nextInt();
+        } while(minutesStayed<0 || minutesStayed>60);
+
+        if (minutesStayed == 0) {
+            amountToPay = ratePerHour*hoursStayed;
+        } else {
+            amountToPay = ratePerHour*(hoursStayed+1);
+        }
 
         System.out.printf("El monto que debes pagar es de %d MXN.", amountToPay);
+        scannedTimeStayed.close();
     }
 }
