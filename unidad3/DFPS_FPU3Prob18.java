e608ebaa7123791f3e6787c8c1f95d74e58d741f
kosail <kamururi@pm.me>
2023-10-24 13:25:08 -0600

    last corrections on this unit, sadly

--- unidad3/DFPS_FPU3Prob18.java
+++ unidad3/DFPS_FPU3Prob18.java
@@ -1,7 +1,32 @@
+// Leer dos números enteros y mostrar todos los múltiplos de 5 comprendidos entre el menor y el mayor.
 package unidad3;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class DFPS_FPU3Prob18 {
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws java.io.IOException {
+	int num1=0, num2=0;
+
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	System.out.println("Ingresa dos números, uno a la vez: ");
+	num1 = Integer.parseInt(br.readLine());
+	num2 = Integer.parseInt(br.readLine());
+
+	if (num2>num1) {
+		for (int i=num1;i<num2;i++) {
+			if (i%5==0) {
+				System.out.print(i + " ");
+			}
+		}
+	} else {
+		for (int i=num2;i<num1;i++) {
+			if (i%5==0) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+	System.out.println("");
+	br.close();
 	}
 }
--- unidad3/DFPS_FPU3Prob6.java
+++ unidad3/DFPS_FPU3Prob6.java
@@ -1,7 +1,38 @@
+// Leer un número entero de tres dígitos y mostrar todos los enteros comprendidos entre 1 y cada uno de los dígitos.
 package unidad3;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class DFPS_FPU3Prob6 {
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws java.io.IOException {
+		int num=0;
+		int[] digitsArray = new int[3];
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		do {
+			System.out.print("Ingresa un número entero de tres dígitos: ");
+			num = Integer.parseInt(br.readLine());
+		}while(Math.abs(num)<100 || Math.abs(num)>999);
+
+		digitsArray[0] = (int) num/100;
+		digitsArray[1] = (int) (num/10)%10;
+		digitsArray[2] = num%10;
+
+		for (int i=0;i<3;i++) {
+			System.out.printf("Dígito %d: ", digitsArray[i]);
+
+			if (num>0) {
+				for (int j=1;j<=digitsArray[i];j++) {
+					System.out.print(j + " ");
+				}
+			} else {
+				for (int j=1;j>=digitsArray[i];j--) {
+					System.out.print(j + " ");
+				}
+			}
+			System.out.println("");
+		}
+		br.close();
 	}
 }
