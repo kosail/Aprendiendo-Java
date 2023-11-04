9a09546535c4c8561e60813521e08a47d0c9bb2c
kosail <kamururi@pm.me>
2023-11-04 08:24:40 -0600

    Updating some exercises resolved

--- unidad4/DFPS_FPU4Prob20.java
+++ unidad4/DFPS_FPU4Prob20.java
@@ -1,7 +1,37 @@
+// Leer 10 números enteros, almacenarlos en un vector y determinar en qué posición está el menor número primo.
 package unidad4;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class DFPS_FPU4Prob20 {
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] numbers = new int[10];
+		int lowestNumber=1, positionLowestNumber=0;
+		boolean isPrime;
+
+		System.out.println("Ingresa 10 números enteros, uno a la vez: ");
+		for (int i=0;i<numbers.length;i++) {
+			numbers[i] = Integer.parseInt(br.readLine());
+		
+			isPrime = true;
+			if ((numbers[i]&1)!=0) {
+				for (int j=2;j<numbers[i]/2;j++) {
+					if (numbers[i]%j==0) {
+						isPrime = false;
+						break;
+					}
+				}
+
+				if (lowestNumber>=numbers[i] && isPrime) {
+					lowestNumber = numbers[i];
+					positionLowestNumber = i + 1; // Añadiendo 1 para que el output sea más comprensible para los humanos.
+				}
+			}
+		}
+		System.out.println("Posición del menor número primo: " + positionLowestNumber);
+		br.close();
 	}
 }
--- unidad4/DFPS_FPU4Prob22.java
+++ unidad4/DFPS_FPU4Prob22.java
@@ -1,7 +1,26 @@
+// Leer 12 números enteros, almacenarlos en un vector y determinar cuáles son los números múltiplos de 5 y en qué posiciones están.
 package unidad4;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class DFPS_FPU4Prob22 {
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] numbers = new int[12];
+		String positionsFiveMultipliers = "";
+
+		System.out.println("Ingresa 12 números enteros, uno por uno:");
+		for (int i=0;i<numbers.length;i++) {
+			numbers[i] = Integer.parseInt(br.readLine());
+
+			if (numbers[i]%5==0) {
+				positionsFiveMultipliers = positionsFiveMultipliers.concat((i+1) + " "); // Añadiendo 1 para hacerlo más amigable con la forma de contar de los humanos.
+			}
+		}
+
+		System.out.println("Multiplos de 5 encontrados en las posiciones: " + positionsFiveMultipliers);
+		br.close();
 	}
 }
